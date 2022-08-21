c4eabf21deb38357dec08618340dd87fce6b0b2f
natogourmet <natogourmet@hotmail.com>
2022-08-20 22:44:46 -0500

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+console.clear();
+console.log('=====================');
+console.log('Hello World!');
+console.log('=====================');
+
+const base = 5;
+
+for (let index = 1; index <= 10; index++) {
+  console.log(`${base} * ${index} = ${base * index}`);
+}
