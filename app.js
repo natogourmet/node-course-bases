5d7757e819c010576a7d432661684b7bc3cc93bd
natogourmet <natogourmet@hotmail.com>
2022-08-20 22:50:51 -0500

    require

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
+
 console.clear();
 console.log('=====================');
 console.log('Hello World!');
 console.log('=====================');
 
 const base = 5;
+let output = '';
 
 for (let index = 1; index <= 10; index++) {
-  console.log(`${base} * ${index} = ${base * index}`);
+  output += `${base} * ${index} = ${base * index}\n`;
 }
+
+fs.writeFile( 'output.txt', output, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
