35fed06fb1b80d458b60ed38ed25701d64b34fb8
natogourmet <natogourmet@hotmail.com>
2022-08-20 23:19:41 -0500

    install packages

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const { createFile } = require('./helpers/multiply');
-
 console.clear();
 
+
+
 const base = 5;
-createFile(base)
-  .then((fileName) => console.log(fileName, 'created'))
-  .catch((err) => console.log(err));
+// createFile(base)
+//   .then((fileName) => console.log(fileName, 'created'))
+//   .catch((err) => console.log(err));
